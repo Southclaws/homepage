a163ac793ef2cacd38a26fe034c5ea5479e82c6f
Barnaby Keene <hello@southcla.ws>
2019-03-30 18:19:32 +0000

    removed dynamic JSON-LD data loading

--- pages/App.js
+++ pages/App.js
@@ -6,62 +6,24 @@ import About from "./components/About";
 import Socials from "./components/Socials";
 import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
-import DefaultData from "./Default";
+import { ld, portfolio } from "./Data";
 
 import "./App.css";
 import "./components/css/Layout.css";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = DefaultData;
-    }
-
-    async componentDidMount() {
-        let data = undefined;
-
-        try {
-            data = await this.getPortfolioData();
-            this.setState({
-                ld: data.ld,
-                portfolio: data.portfolio
-            });
-        } catch (e) {
-            console.log(
-                "could not get portfolio JSON data from GitHub Gist:",
-                e,
-                "using hard-coded default instead (may be outdated)"
-            );
-        }
-    }
-
-    async getPortfolioData() {
-        let response = await fetch("https://api.github.com/gists/f008b76671d2e961027281aa6511fb1a");
-        if (response.status !== 200) {
-            throw "failed to fetch portfolio data";
-        }
-
-        let payload = await response.json();
-
-        return await JSON.parse(payload.files.homepage.content);
-    }
-
-    render() {
-        return (
-            <div>
-                <JsonLD data={this.state.ld} />
-                <Header image={this.state.ld.image} />
-                <hr />
-                <About description={this.state.ld.description} />
-                <hr />
-                <Socials />
-                <hr />
-                <Portfolio items={this.state.portfolio} />
-                <hr />
-                <Footer />
-            </div>
-        );
-    }
+export default function App() {
+    return (
+        <div>
+            <JsonLD data={ld} />
+            <Header image={ld.image} />
+            <hr />
+            <About description={ld.description} />
+            <hr />
+            <Socials />
+            <hr />
+            <Portfolio items={portfolio} />
+            <hr />
+            <Footer />
+        </div>
+    );
 }
-
-export default App;
